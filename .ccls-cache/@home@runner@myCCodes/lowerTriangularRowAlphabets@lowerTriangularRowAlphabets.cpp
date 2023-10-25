c6a9af193e0164ff541cd07fb3796f86bc2386ae
onkarsarje <73869353+onkarsarje@users.noreply.github.com>
2023-10-25 12:20:18 +0000

    updated something

--- .ccls-cache/@home@runner@myCCodes/lowerTriangularRowAlphabets@lowerTriangularRowAlphabets.cpp
+++ .ccls-cache/@home@runner@myCCodes/lowerTriangularRowAlphabets@lowerTriangularRowAlphabets.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
+  char txt='a';
   for(int row=1;row<=5;row++){
-    for(int col=row;col>0;col--){
-      cout<<col<<" ";
-    }
+    for(int col=1;col<=row;col++){
+      cout<<txt<<" ";
+    }    
+    txt++;
     cout<<endl;
   }
 }
---
+++ lowerTriangularReverse/main2.cpp
@@ -0,0 +1,10 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   for(int row=1;row<=5;row++){
+//     for(int col=row;col>0;col--){
+//       cout<<col<<" ";
+//     }
+//     cout<<endl;
+//   }
+// }
