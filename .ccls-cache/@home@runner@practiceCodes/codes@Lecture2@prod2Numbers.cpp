e2ab42c2d625f6e2b6e82f3cbae83c23efa65d19
onkarsarje <73869353+onkarsarje@users.noreply.github.com>
2023-10-26 20:00:45 +0000

    Product of two numbers

---
+++ .ccls-cache/@home@runner@practiceCodes/codes@Lecture2@prod2Numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  float num1, num2, prod;
+  cout<<"Product of two numbers.\nEnter First number: ";
+  cin>>num1;
+  cout<<"Enter Second Number: ";
+  cin>>num2;
+  prod=num1*num2;
+  cout<<"The Product of two numbers is: "<<prod;
+}
