ed425e35157104f5970479a7e8a8b8d68c844b47
onkarsarje <73869353+onkarsarje@users.noreply.github.com>
2023-10-26 19:35:19 +0000

    Sum of two numbers.

---
+++ codes/sum2Numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"Sum of two numbers.\nEnter First number: ";
+  float num1,num2,sum;
+  cin>>num1;
+  cout<<"Enter Second number: ";
+  cin>>num2;
+  sum=num1+num2;
+  cout<<"The Sum of Two numbers is: "<<sum<<endl;
+}
