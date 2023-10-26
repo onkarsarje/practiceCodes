55064e8fcbf8e2819afd27cb0ac07d139a4190a5
onkarsarje <73869353+onkarsarje@users.noreply.github.com>
2023-10-26 21:00:35 +0000

    Square of a Number

---
+++ codes/Lecture2/squareOfNumber.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+  double num, square;
+  cout<<"Square of a number.\nEnter a number: ";
+  cin>>num;
+  square=num*num;
+  cout<<"The Square of the number is: "<<square<<endl;
+}
