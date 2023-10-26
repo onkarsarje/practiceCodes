e075d6eafdcc810f8064859496de5781d01c3ced
onkarsarje <73869353+onkarsarje@users.noreply.github.com>
2023-10-26 19:47:13 +0000

    Difference of two numbers

---
+++ codes/diff2Numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  float num1, num2, diff;
+  cout<<"Difference of two numbers.\nEnter First Number: ";
+  cin>>num1;
+  cout<<"Enter Second Number: ";
+  cin>>num2;
+  diff=num1-num2;
+  cout<<"On subtracting second number from the first number: "<<diff;
+}
