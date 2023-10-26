51138c20ab18a0754589229d8c6fe5515181305e
onkarsarje <73869353+onkarsarje@users.noreply.github.com>
2023-10-26 20:26:47 +0000

    Average of two numbers

---
+++ codes/Lecture2/avg2Numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  float num1, num2, avg;
+  cout<<"Average of Two numbers.\nEnter First Number: ";
+  cin>>num1;
+  cout<<"Enter Second Number: ";
+  cin>>num2;
+  avg=(num1+num2)/2;
+  cout<<"The Average of Two numbers is: "<<avg;
+}
