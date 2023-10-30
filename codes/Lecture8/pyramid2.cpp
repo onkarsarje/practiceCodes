bf60576e86367bcf90301ae18acd6eeceec28ac6
onkarsarje <73869353+onkarsarje@users.noreply.github.com>
2023-10-30 21:22:09 +0000

    pyramid2

--- codes/Lecture8/pyramid2.cpp
+++ codes/Lecture8/pyramid2.cpp
@@ -10,12 +10,21 @@ int main(){
   int n;
   cout<<"Enter the number of rows: ";
   cin>>n;
+  // for(int row=1;row<=n;row++){
+  //   for(int col=n;col>row;col--){
+  //     cout<<"  ";
+  //   }
+  //   for(int col=1;col<=2*row-1;col++){
+  //     cout<<"* ";
+  //   }
+  //   cout<<endl;
+  // }
   for(int row=1;row<=n;row++){
-    for(int col=n;col>row;col--){
-      cout<<"  ";
+    for(int col=1;col<=n-row;col++){
+      cout<<" ";
     }
-    for(int col=1;col<=2*row-1;col++){
-      cout<<"* ";
+    for(int col=1;col<=(2*row)-1;col++){
+      cout<<"*";
     }
     cout<<endl;
   }
